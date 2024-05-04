5c105d6f664754547ce9661d906353e977d9cb98
Ameyabarve123 <barveameya@yahoo.com>
2024-05-04 16:37:37 -0700

    Update main.py

--- YoutubeToMP3Converter/main.py
+++ YoutubeToMP3Converter/main.py
@@ -7,11 +7,7 @@ import os
 yt = YouTube(input("Enter the URL: "))
 
 
-#destination = r"C:\Users\barve\OneDrive\Desktop\SpotifyDownloads"
 destination = filedialog.askdirectory()
-
-#the r is neccessary, so this is a raw string... because its backslashes and not forward slashes, we need that r
-
 
 video = yt.streams.get_highest_resolution()
 
@@ -27,7 +23,8 @@ mp3file= input('\nSong Title: ')
 videoclip = VideoFileClip(out_file)
 audioclip = videoclip.audio
 audioclip.write_audiofile(destination + '\\' + mp3file + '.mp3')
-#have to have the ".mp3" because it tells the write_audiofile what type of audio to be written as... ex: audio types could be WAV, M4A, MP3
+#have to have the ".mp3" because it tells the write_audiofile what type of audio to be written as... 
+#ex: audio types could be WAV, M4A, MP3
 
 audioclip.close()
 videoclip.close()
